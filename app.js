b2388463db63e5108fa7dbace45e46b8cf1a5908
Nate Kerksick <nate@culturegraphic.com>
2018-09-09 16:15:47 -0700

    Change temp message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('culturegraphic.com temporary maintenance');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
